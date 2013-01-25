838fdde501463ea8912fabf6c6a247db2606a0c6
TraberSoftware <iegames.tk@gmail.com>
2013-01-25 23:44:29 +0100

    Improved column listing from tables

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
 $dbh=mysql_connect ("localhost", "root", "") or die ('No se ha podido conectar debido a un error en la base de datos: ' . mysql_error()); 
+//mysql_select_db ("practica_1_php"); 
 ?>
